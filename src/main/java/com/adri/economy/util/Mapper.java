f8f2232d7e98112b83574079d7c9511734388a1a
Adria <adriamartinezfogmail.com>
2020-03-15 00:41:00 +0100

    Global

--- src/main/java/com/adri/economy/util/Mapper.java
+++ src/main/java/com/adri/economy/util/Mapper.java
@@ -1,11 +1,7 @@
 package com.adri.economy.util;
 
-import com.adri.economy.dto.AccountDTO;
-import com.adri.economy.dto.AppUserDTO;
-import com.adri.economy.dto.RoleDTO;
-import com.adri.economy.model.Account;
-import com.adri.economy.model.AppUser;
-import com.adri.economy.model.Role;
+import com.adri.economy.dto.*;
+import com.adri.economy.model.*;
 import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
@@ -52,6 +48,42 @@ public class Mapper {
                 .creationDate(account.getCreationDate())
                 .deletedDate(account.getDeletedDate())
                 .owner(account.getOwner() == null ? null : mapToAppUserDTO(account.getOwner()))
+                .build();
+    }
+
+    public static AccountDTO mapToAccountDTO(Account account, AccountBalance accountBalance){
+        AccountDTO accountDTO = mapToAccountDTO(account);
+
+        accountDTO.setCurrentBalance(AccountBalanceDTO.builder()
+                .date(accountBalance.getDate())
+                .balance(accountBalance.getBalance())
+                .build());
+
+        return accountDTO;
+    }
+
+    public static OperationDTO mapToOperationDTO(Operation operation){
+        return OperationDTO.builder()
+                .id(operation.getId())
+                .description(operation.getDescription())
+                .info(operation.getInfo())
+                .date(operation.getDate())
+                .amount(operation.getAmount())
+                .currencyAmount(operation.getCurrencyAmount())
+                .currency(operation.getCurrency())
+                .accountId(operation.getAccount().getId())
+                .category(operation.getCategory() == null ? null : mapToOperationCategoryDTO(operation.getCategory()))
+                .build();
+    }
+
+    public static OperationCategoryDTO mapToOperationCategoryDTO(OperationCategory operationCategory){
+        return OperationCategoryDTO.builder()
+                .id(operationCategory.getId())
+                .name(operationCategory.getName())
+                .description(operationCategory.getDescription())
+                .creationDate(operationCategory.getCreationDate())
+                .deletedDate(operationCategory.getDeletedDate())
+                .icon(operationCategory.getIcon())
                 .build();
     }
 }
